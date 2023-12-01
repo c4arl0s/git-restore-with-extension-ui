b77f662dd421906521d29368a4041c96af1ee72e
c4arl0s <c.santiago.cruz@gmail.com>
2023-11-30 21:53:03 -0600

    Restring correctly to find files with an specific extension

--- git-restore-with-filter-ui.sh
+++ git-restore-with-filter-ui.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-PARAMETER=$1
-TRACKED_FILES=$(git --no-pager diff --name-only --cached --diff-filter=AM | grep $PARAMETER)
+EXTENSION=${1:?"Error: The first parameter is missign, It should be an extension."}
+TRACKED_FILES=$(git --no-pager diff --name-only --cached --diff-filter=AM | grep ".*.${EXTENSION}$")
 
-FILES_WITH_FILTER="Tracked files to unstaged with filter ($PARAMETER):"
-ERROR_MSG="Tracked files with filter: $PARAMETER don't exist"
+FILES_WITH_EXTENSION="Tracked files to unstaged with extension ($EXTENSION):"
+ERROR_MSG="Tracked files with extension: $EXTENSION don't exist"
 
 if [[ $TRACKED_FILES ]]; then
     let COUNTER=0
-    LINE=$(git --no-pager diff --name-only --cached --diff-filter=AM | grep $PARAMETER |
+    LINE=$(git --no-pager diff --name-only --cached --diff-filter=AM | grep ".*.${EXTENSION}$" |
            while read TRACKED_FILE
            do
                let "COUNTER+=1"
@@ -16,11 +16,10 @@ if [[ $TRACKED_FILES ]]; then
            done
           )
     echo $LINE;
-    SELECTED_TRACKED_FILES=$(echo $LINE | 
+    SELECTED_STAGED_FILES=$(echo $LINE | 
                              xargs dialog --stdout --checklist $FILES_WITH_FILTER 0 0 0
                             )
-    echo $SELECTED_TRACKED_FILES | xargs git restore --staged
+    [ ! -z "$SELECTED_STAGED_FILES" ] && echo $SELECTED_STAGED_FILES | xargs git restore --staged || echo "🟡 You did not select any file to restore with"
 else
     echo  $ERROR_MSG
 fi
-
